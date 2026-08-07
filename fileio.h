640e4badbab682044590bb4f437530f5ab2231f6
Quazim <oquadri2147@conestogac.on.ca>
2026-08-06 20:32:25 -0400

    Fix: Updated saveToFile and loadFromFile signatures to match fileio.c (void return type alignment)

--- fileio.h
+++ fileio.h
@@ -4,9 +4,9 @@
 #include "inventory.h"
 
 // Saves inventory to a file
-int saveToFile(const char *filename, InventoryItem *list, int count);
+void saveToFile(const char *filename, InventoryItem *list, int count);
 
 // Loads inventory from a file
-int loadFromFile(const char *filename, InventoryItem **list, int *count);
+void loadFromFile(const char *filename, InventoryItem **list, int *count);
 
 #endif
